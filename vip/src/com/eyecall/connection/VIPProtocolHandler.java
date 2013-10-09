549273ee75caa301f18d0f2a3cd1f53d63d9a90d
denniss17 <dennisschroer@hotmail.com>
2013-10-09 17:31:31 +0200

    Zo...

---
+++ vip/src/com/eyecall/connection/VIPProtocolHandler.java
@@ -0,0 +1,13 @@
+package com.eyecall.connection;
+
+import java.util.Map;
+
+public class VIPProtocolHandler implements ProtocolHandler {
+
+	@Override
+	public State handleMessage(State state, String name, Map<String, Object> params) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
