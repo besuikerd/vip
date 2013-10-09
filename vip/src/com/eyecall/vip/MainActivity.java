5fb1bd5c57b6d99c6031f14a3cfb65950f948119
denniss17 <dennisschroer@hotmail.com>
2013-10-09 14:30:46 +0200

    Test

--- vip/src/com/eyecall/vip/MainActivity.java
+++ vip/src/com/eyecall/vip/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    	// test 123
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
