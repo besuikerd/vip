f5ebda3dc45174ddaf0bb48bb09a8983d3ef9720
Nick ten Veen <nveen0930@gmail.com>
2013-10-15 09:34:10 +0200

    implemented Connection

--- server/src/com/eyecall/connection/test/TestProtocolHandler.java
+++ server/src/com/eyecall/connection/test/TestProtocolHandler.java
@@ -1,33 +1,53 @@
 package com.eyecall.connection.test;
 
-import java.util.Map;
+import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.eyecall.connection.Message;
+import com.eyecall.connection.OutQueue;
 import com.eyecall.connection.ProtocolHandler;
 import com.eyecall.connection.State;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class TestProtocolHandler implements ProtocolHandler{
+public class TestProtocolHandler implements ProtocolHandler<TestState>{
+	private static final Logger logger = LoggerFactory.getLogger(TestProtocolHandler.class);
+	private String name;
+	
 	@Override
-	public State handleMessage(State state, String name,
-			Map<String, Object> params) {
-		TestState s = (TestState) state;
+	public State handleMessage(TestState state, Message m, OutQueue<Message> queue) {
 		
-		
-		switch(s){
+		switch(state){
 		case AWAITING_CONNECTION:
-			
-			if(params.containsKey("name")){
-				
-			} else{
-				
-				return TestState.AWAITING_CONNECTION;
+			logger.debug("message received while awaiting connection: {}", m);
+			switch(m.getName()){
+			case "hello":
+				if(m.hasParam("hello")){
+					this.name = m.getParam("hello").toString();
+					queue.add(new Message("hello").add("hello", "otherside"));
+					return TestState.CONNECTED;
+				}
+			default:
+				return null;
 			}
 			
 			
-			break;
 		case CONNECTED:
-			break;
+			logger.debug("message received from {} while connected: {}", name, m.toString());
+			if(m.hasParam("close") && m.getParam("close", Boolean.class)){
+				
+				return TestState.DISCONNECTED;
+			}
+			return TestState.CONNECTED;
+			
 		case DISCONNECTED:
-			break;
+			logger.debug("message received after disconnect: {}", m.toString());
+			return TestState.DISCONNECTED;
 		}
 		
 		return null;
