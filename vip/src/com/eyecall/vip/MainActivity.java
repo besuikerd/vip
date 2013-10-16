875649b2f07cc3f74beef8efab5747f690532a4a
denniss17 <dennisschroer@hotmail.com>
2013-10-16 17:24:23 +0200

    Location stuff

--- vip/src/com/eyecall/vip/MainActivity.java
+++ vip/src/com/eyecall/vip/MainActivity.java
@@ -1,23 +1,139 @@
 package com.eyecall.vip;
 
+import com.eyecall.connection.Connection;
+import com.eyecall.connection.Message;
+import com.eyecall.connection.ProtocolHandler;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Toast;
 
+/**
+ * The main Activity of this Applications
+ */
+public class MainActivity extends Activity implements GooglePlayServicesClient.ConnectionCallbacks,
+GooglePlayServicesClient.OnConnectionFailedListener {
+	
+	/*
+     * Define a request code to send to Google Play services
+     * This code is returned in Activity.onActivityResult
+     */
+    private final static int
+            CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 
-public class MainActivity extends Activity {
 	
 	public static final String TAG = "Eyecall VIP";
+	public static Connection connection;
+	/** LocationClient used for getting last known location */
+	private LocationClient locationClient;
 	
-	@Override
-	public void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState);
-		this.openHelpActivity();
+    @Override
+    protected void onStart() {
+        super.onStart();
+        
+        // Determine current location via Google Services
+        locationClient = new LocationClient(this, this, this);
+        // Connect the client.
+        locationClient.connect();
+        Location location = locationClient.getLastLocation();
+	    
+        // Initialize connection with the server
+		initConnection();
+		
+		// Send help request
+		connection.send(new Message(ProtocolHandler.REQUEST_HELP).add("longitude", location.getLongitude()).add("latitude", location.getLatitude()));
+		
+		// Keep screen on
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		
+        // And wait for response ... 
+		
+		//this.openHelpActivity();
+    }
+
+    @Override
+    protected void onStop() {
+        // Disconnecting the client invalidates it.
+        locationClient.disconnect();
+        super.onStop();
+    }
+
+	
+	/**
+	 * Initialize the connection with the server
+	 * @ensure this.connection!=null
+	 */
+	private void initConnection() {
+		// TODO Auto-generated method stub
 	}
-	
+
 	public void openHelpActivity(){
 		Intent intent = new Intent(getApplicationContext(), HelpActivity.class);
 		this.startActivity(intent);
 	}
+
+	/*
+     * Called by Location Services when the request to connect the
+     * client finishes successfully. At this point, you can
+     * request the current location or start periodic updates
+     */
+    @Override
+    public void onConnected(Bundle dataBundle) {
+        // Display the connection status
+        Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+    }
+    
+    /*
+     * Called by Location Services if the connection to the
+     * location client drops because of an error.
+     */
+    @Override
+    public void onDisconnected() {
+        // Display the connection status
+        Toast.makeText(this, "Disconnected. Please re-connect.", Toast.LENGTH_SHORT).show();
+    }
+    
+    /*
+     * Called by Location Services if the attempt to
+     * Location Services fails.
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(
+                        this,
+                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+            showDialog(connectionResult.getErrorCode());
+        }
+    }
+
 	
 }
