9bb489d4a8fc319063640b5266bfd3ce41c47ccf
denniss17 <dennisschroer@hotmail.com>
2013-10-18 14:47:18 +0200

    Connection aanmaken; Hulpverzoek versturen

--- vip/src/com/eyecall/vip/VIPState.java
+++ vip/src/com/eyecall/vip/VIPState.java
@@ -5,5 +5,19 @@ public enum VIPState implements State {
 	IDLE,
 	WAITING,
 	BEING_HELPED,
-	DISCONNECTED
+	DISCONNECTED(true);
+
+	private boolean isTerminal = false;
+
+	private VIPState(boolean isTerminal) {
+		this.isTerminal = isTerminal;
+	}
+	
+	private VIPState() {
+	}
+	
+	@Override
+	public boolean isTerminal() {
+		return isTerminal;
+	}
 }
