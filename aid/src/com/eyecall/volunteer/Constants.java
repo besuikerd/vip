4a2fcced1e61938606b9795b50c3343e208a9cd7
Nick ten Veen <nveen0930@gmail.com>
2013-11-25 15:57:02 +0100

    edited camera, it's broken now! :(

--- aid/src/com/eyecall/volunteer/Constants.java
+++ aid/src/com/eyecall/volunteer/Constants.java
@@ -2,6 +2,6 @@ package com.eyecall.volunteer;
 
 public class Constants {
 	//public static final String SERVER_URL = "www.besuikerd.nl";
-	public static final String SERVER_URL = "130.89.179.60";
+	public static final String SERVER_URL = "80.100.129.128";
 	public static final int SERVER_PORT = 5000;
 }
