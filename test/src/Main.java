aa195f4d5b3e4559e10c4d184e7afa541382e00b
denniss17 <dennisschroer@hotmail.com>
2014-01-13 15:50:15 +0100

    Test

---
+++ test/src/Main.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.eyecall.vip.VIPProtocolHandler;
+import com.eyecall.volunteer.VolunteerProtocolHandler;
+
+
+public class Main {
+	
+	private VIPProtocolHandler vip;
+	private VolunteerProtocolHandler volunteer;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
